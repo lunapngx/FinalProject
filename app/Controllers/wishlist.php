e83087e7169b45fa8d98e07b41f49780e72d001e
Syrafim123456789012345678901234567890 <giamostajo23@gmail.com>
2025-07-10 13:29:14 +0800

    update all

---
+++ app/Controllers/wishlist.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Controllers;
+
+
+class wishlist extends BaseController
+{
+public function wishlist()
+{
+return view('wishlist'); // or 'Customer/wishlist' if it's inside that folder
+}
+}
